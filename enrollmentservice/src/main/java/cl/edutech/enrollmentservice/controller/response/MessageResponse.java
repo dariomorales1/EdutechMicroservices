2e10fe1052f920c43b38aaf497646b44f1013b7d
dariomorales1 <dar.moralesa@duocuc.cl>
2025-05-05 22:09:55 -0400

    Creacion del crud de enrollmentService

--- enrollmentservice/src/main/java/cl/edutech/enrollmentservice/controller/response/MessageResponse.java
+++ enrollmentservice/src/main/java/cl/edutech/enrollmentservice/controller/response/MessageResponse.java
@@ -1,4 +1,12 @@
 package cl.edutech.enrollmentservice.controller.response;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class MessageResponse {
+    private String message;
 }
