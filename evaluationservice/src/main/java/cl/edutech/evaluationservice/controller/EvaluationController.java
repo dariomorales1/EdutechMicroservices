7bdd2989a1546b3710803696e57ebe91d0b35509
Bastian <ba.conchar@duocuc.cl>
2025-05-08 00:19:55 -0400

    registro de microservice de evaluation service 3

--- evaluationservice/src/main/java/cl/edutech/evaluationservice/controller/EvaluationController.java
+++ evaluationservice/src/main/java/cl/edutech/evaluationservice/controller/EvaluationController.java
@@ -1,9 +1,25 @@
 package cl.edutech.evaluationservice.controller;
 
 import cl.edutech.evaluationservice.controller.Response.MessageResponse;
+import cl.edutech.evaluationservice.service.EvaluationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
-
-
+@RestController
+@RequestMapping("/evaluations")
 public class EvaluationController {
+    @Autowired
+    private EvaluationService evaluationService;
+
+    @GetMapping("/ping")
+    public ResponseEntity<MessageResponse> ping() {return ResponseEntity.ok(new MessageResponse("PONG"));
+    }
+
+
+
+
 }
